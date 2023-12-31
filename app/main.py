1e12554fcea1cf42c2ac85977e4d3e53479b90fc
Diego Rojas <diego.rojas@millicom.com>
2023-12-31 10:10:56 +1000

    The code was cleaned a bit

--- app/main.py
+++ app/main.py
@@ -2,14 +2,11 @@ from fastapi import FastAPI
 import uvicorn 
 from app.api.login.service.routes import login
 from app.api.users.service.routes import users
-from app.frontend.login.routes import loginWeb
-from app.frontend.users.routes import usersWeb
+
 
 app = FastAPI()
 app.include_router(users)
 app.include_router(login)
-app.include_router(loginWeb)
-app.include_router(usersWeb)
 
 ##
 ##app.mount(Routes.REGISTER, paquetes_app)  # /api/v1.0.0/signup
